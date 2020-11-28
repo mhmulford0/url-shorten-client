6aa2b4c63c447e24ead232f227e0a797c99d352f
Matthew Mulford <mhmulford0@gmail.com>
2020-11-28 12:51:47 -0600

    added /info index page and passing info to look up route

---
+++ pages/info/index.js
@@ -0,0 +1,57 @@
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { useState } from 'react';
+
+import {
+  Container,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+  Input,
+  Button,
+  Heading,
+} from '@chakra-ui/react';
+import Navbar from '../../components/Navbar';
+
+export default function index() {
+  const [linkInfo, setLinkInfo] = useState('');
+  const router = useRouter();
+
+  const handleChange = (e) => {
+    setLinkInfo(e.target.value);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    router.push(`/info/${linkInfo}`);
+  };
+  return (
+    <>
+      <Navbar />
+      <Container>
+        <Heading my="10px" textAlign="center">
+          Link Info
+        </Heading>
+        <form>
+          <FormControl id="link-info">
+            <FormLabel>Short Link</FormLabel>
+            <Input type="text" value={linkInfo} onChange={handleChange} />
+            <FormHelperText>
+              Must be the short link code provided
+            </FormHelperText>
+            <Button mb="20px" onClick={submitHandler}>
+              Get Info
+            </Button>
+          </FormControl>
+        </form>
+      </Container>
+    </>
+  );
+}
+
+export async function getStaticProps(context) {
+  return {
+    props: {},
+  };
+}
