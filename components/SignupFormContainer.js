e78c73e3b48af8cb761812408492bc4d3f1a6176
Matthew Mulford <mhmulford0@gmail.com>
2021-01-12 16:13:20 -0600

    missed from last commit

--- components/SignupFormContainer.js
+++ components/SignupFormContainer.js
@@ -1,19 +1,5 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';
-const firebaseConfig = {
-  apiKey: 'AIzaSyCgO-w3WVqCwUiKCPfNbOuqa-fofw3W1_k',
-  authDomain: 'lnkshrt-5cc68.firebaseapp.com',
-  projectId: 'lnkshrt-5cc68',
-  storageBucket: 'lnkshrt-5cc68.appspot.com',
-  messagingSenderId: '17155075282',
-  appId: '1:17155075282:web:24d3e5b160694b4871910b',
-};
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
-
 function SignupForm() {
-  return <SignupForm firebase={firebase} />;
+  return <SignupForm />;
 }
 
 export default SignupForm;
